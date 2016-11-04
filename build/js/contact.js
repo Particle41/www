de4779d5fcfef5715f1b21f74f92b6805eea3970
gabethexton <gabethexton@gmail.com>
2016-11-04 00:56:05 -0600

    clear contact form working

--- build/js/contact.js
+++ build/js/contact.js
@@ -1,33 +1,37 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-var URL = 'https://7s32rh6p7k.execute-api.us-west-2.amazonaws.com/dev/contact';
+    var URL = 'https://7s32rh6p7k.execute-api.us-west-2.amazonaws.com/dev/contact';
 
 
-  $('#contact-form').submit(function (event) {
-    event.preventDefault();
-    var data = {
-      name: $('#name').val(),
-      email: $('#email').val(),
-      phone: $('#phone').val(),
-      message: $('#message').val()
-  };
+    $('#contact-form').submit(function (event) {
+        event.preventDefault();
+        var data = {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            phone: $('#phone').val(),
+            message: $('#message').val()
+        };
 
-    $.ajax({
-      type: 'POST',
-      url: URL,
-      dataType: 'json',
-      contentType: 'application/json',
-      data: JSON.stringify(data),
-      success: function () {
-          console.log('Successfully sent email.');
-        // clear form and show a success message
-      },
-      error: function () {
-          console.log('Failed to send email.');
-        // show an error message
-      }
-  });
-  });
+        $.ajax({
+            type: 'POST',
+            url: URL,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function () {
+                // TODO: Show a success message!
+                console.log('Successfully sent email.');
+                $('#contact-form').find("#name").val("");
+                $('#contact-form').find("#email").val("");
+                $('#contact-form').find("#phone").val("");
+                $('#contact-form').find("#message").val("");
+            },
+            error: function () {
+                console.log('Failed to send email.');
+                // TODO: show an error message
+            }
+        });
+    });
 });
