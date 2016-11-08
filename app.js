a529f0510c78aa5f06d06cc05e282a4b1c657ba6
gabethexton <gabethexton@gmail.com>
2016-11-08 14:29:45 -0700

    testing as basic express app

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req,res) {
+    res.render('home');
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Node running on port", app.get('port'));
+});
