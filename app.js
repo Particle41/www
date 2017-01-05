7694d194a1424f5a430e5c707559c03c37e73fe5
Gabe Thexton <gabethexton@gmail.com>
2017-01-05 16:30:48 -0700

    more s3 upload code

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const aws = require('aws-sdk');
+
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('build'));
+
+app.engine('html', require('ejs').renderFile);)
 
 app.get('/', function(req, res) {
     res.sendfile('build/home.html');
@@ -14,3 +18,5 @@ app.get('/', function(req, res) {
 app.listen(app.get('port'), function () {
     console.log("Node running on port", app.get('port'));
 });
+
+const S3_BUCKET = process.env.S3_BUCKET;
