8e3e2b9f02c7181059a91db8ced64261ec53ab98
Gabe Thexton <gabethexton@gmail.com>
2017-01-05 22:25:03 -0700

    syntax error and refactoring

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 
 const express = require('express');
 const aws = require('aws-sdk');
+const S3_BUCKET = process.env.S3_BUCKET;
+require('dotenv').config();
 
 const app = express();
-require('dotenv').config();
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('build'));
 
-app.engine('html', require('ejs').renderFile);)
-
 app.get('/', function(req, res) {
     res.sendfile('build/home.html');
 });
 
-app.listen(app.get('port'), function () {
+app.engine('html', require('ejs').renderFile);
+
+app.listen(app.get('port'), function() {
     console.log("Node running on port", app.get('port'));
 });
-
-const S3_BUCKET = process.env.S3_BUCKET;
